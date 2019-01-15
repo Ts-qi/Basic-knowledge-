78b2878eb55fd56c00bbbf21f879672546bd827b
tangqi <1024505996@qq.com>
2019-01-15 16:16:17 +0800

    webpack 的基本配置

--- React-project/webpack.config.js
+++ React-project/webpack.config.js
@@ -1,11 +1,29 @@
 const path = require('path');
-
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 module.exports = {
+    mode: 'development',
     entry: {
         filename: './src/index.js'
     },
     output: {
         filename: 'app.js',
         path: path.resolve(__dirname ,'dist')
-    }
+    },
+    module: {
+        rules: [
+            {
+                test:/\.css$/,
+                use:['style-loader','css-loader']
+            },
+            {
+                test: /\.(png|jpeg|gif|svg|jpg)$/,
+                use: [ 'url-loader']
+            }
+        ]
+    },
+    plugins:[
+        new HtmlWebpackPlugin({
+            title:'react-project',
+        })
+    ]
 }
