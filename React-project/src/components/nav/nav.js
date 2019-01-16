acf531b5d84d33d5884037e43491cbd280382c95
tangqi <1024505996@qq.com>
2019-01-16 16:05:58 +0800

    导航样式

--- React-project/src/components/nav/nav.js
+++ React-project/src/components/nav/nav.js
@@ -1,18 +1,39 @@
 import React from 'react';
-import { Button } from 'antd';
+import { Menu, Icon, Switch } from 'antd';
 import './nav.styl';
-
+const SubMenu = Menu.SubMenu;
 class Nav extends React.Component {
     constructor(props) {
         super(props);
+        this.state = {
+            navInfo: [
+                {name:'概况',id:1},
+                {name:'产品',id:2},
+                {name:'设置',id:3},
+                {name:'会员',id:4},
+                {name:'营销',id:5},
+                {name:'店铺',id:6},
+                {name:'数据',id:7},
+            ]
+        }
     }
 
     render () {
+        let { navInfo } = this.state;
         return (
             <div className={'navWarpper'}>
-                <div className={'nav'}>
-                    导航
+                <div className={'navValter'}>
                 </div>
+              
+                {
+                    navInfo && navInfo.length >0 ?
+                    navInfo.map((item,index) => {
+                        return <div className={'navItem'} key={index} >
+                                    { item.name}
+                                </div>
+                    }): null
+
+                }
 
             </div>
         )
